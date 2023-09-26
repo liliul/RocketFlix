48d2ce65f8303d1a3d667d7f6272c9d969f1d3ad
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-25 22:00:59 -0300

    create: utils.js

---
+++ src/services/utils.js
@@ -0,0 +1,8 @@
+// criar tags
+export function createTags(tag) {
+  return document.createElement(tag)
+}
+// add no html
+export function addAppend(id, element) {
+  return id.appendChild(element)
+}
