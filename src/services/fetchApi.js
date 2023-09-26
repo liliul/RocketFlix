661fba793adf172cfa1e860711543b2ed6aa8fdb
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-25 21:50:34 -0300

    chore: jsdoc feat fetchApi

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,3 +1,8 @@
+/**
+* @param {string} url - movies
+* @returns {objetc} - todas informação de um filme 
+*/
+
 export async function fetchApi(url) {
 	const request = await fetch(url);
 	const response = await request.json();
