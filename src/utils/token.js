70ab167837dad2dbea4f34bef4654b7c7b10eb41
liliul <liliuestudo@gmail.com>
2023-10-25 18:44:46 -0300

    create: token.js na pasta utils

---
+++ src/utils/token.js
@@ -0,0 +1,6 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+
+export const API_KEY = 'f8626f6d0de68a83fd4d5966f6b4d27b';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
